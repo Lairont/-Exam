295d2dd4b69e00850787e6d0431003a4f99e6661
Никита <153456515+Lairont@users.noreply.github.com>
2025-06-27 10:39:37 +0300

    Add files via upload

---
+++ ConsoleApp2/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Person : IComparable
+    {
+        private string name;
+        private int age;
+
+        public string Name { get { return name; }
+                             set { name = value; } }
+        //public string Name { get => name; set => name = value; }
+        public int Age { get => age; set => age = value; }
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+       
+        public int CompareTo(object obj)
+        {
+            if (obj is Person other)
+            {
+                // Сравнение по имени
+                return this.Name.CompareTo(other.Name);
+            }
+            return 0;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, Возраст: {Age}";
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> peopleList = new List<Person>
+        {
+            new Person("Андрей", 20),
+            new Person("Максим", 30),
+            new Person("Григорий", 22),
+            new Person("Виктор", 28)
+        };
+
+            foreach (var p in peopleList)
+                Console.WriteLine(p);
+                Console.WriteLine();
+            // Сортировка списка
+            peopleList.Sort();
+
+            Console.WriteLine("Отсортированный список: ");
+            foreach (var p in peopleList)
+                Console.WriteLine(p);
+
+            Console.ReadLine();
+        }
+    }
+}
+
